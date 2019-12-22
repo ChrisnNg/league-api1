e931ef766f9034fc36477df3ab90e20b07e1d0bc
ChrisnNg <cristopherng@hotmail.com>
2019-12-22 07:19:35 -0800

    first static request completed

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const bodyParser = require("body-parser");
 const sass = require("node-sass-middleware");
 const app = express();
 const morgan = require("morgan");
-
+var qs = require("qs");
+const axios = require("axios");
 // PG database client/connection setup
 const { Pool } = require("pg");
 // const dbParams = require('./lib/db.js');
@@ -50,6 +51,34 @@ const widgetsRoutes = require("./routes/widgets");
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/api", (req, res) => {
+  // console.log("post req details", req.body);
+
+  var authOptions = {
+    method: "GET",
+    url:
+      "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/crisang",
+    headers: {
+      Origin: "https://developer.riotgames.com",
+      "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+      "X-Riot-Token": process.env.API_KEY,
+      "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh-TW;q=0.7,zh;q=0.6",
+      "User-Agent":
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36"
+    },
+    json: true
+  };
+  axios(authOptions)
+    .then(function(response) {
+      console.log(response.data);
+      console.log(response.status);
+      res.json(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
 });
 
 app.listen(PORT, () => {
